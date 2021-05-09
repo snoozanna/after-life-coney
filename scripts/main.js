051d41d9108eaa44bf285452a38a02b73c0c5f46
Suzanna <hurstsuzanna@gmail.com>
2021-05-09 20:09:14 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+// Disables right click to prevent from downloading
+
+// const noContext = document.getElementById("player");
+
+// noContext.addEventListener("contextmenu", (e) => {
+//   e.preventDefault();
+// });
+
+const archiveLink = document.getElementById("archiveLink");
+const archiveText = document.getElementById("archiveText");
+archiveLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  archiveText.classList.toggle("archiveText");
+});
+
+const facilitationLink = document.getElementById("facilitationLink");
+const facilitationText = document.getElementById("facilitationText");
+facilitationLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  facilitationText.classList.toggle("facilitationText");
+});
