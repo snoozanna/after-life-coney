64385e72577e654036d6f621da6a01a27f456665
Suzanna <hurstsuzanna@gmail.com>
2021-05-13 11:04:45 +0100

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -6,16 +6,16 @@
 //   e.preventDefault();
 // });
 
-const archiveLink = document.getElementById("archiveLink");
-const archiveText = document.getElementById("archiveText");
-archiveLink.addEventListener("click", (e) => {
-  e.preventDefault();
-  archiveText.classList.toggle("archiveText");
-});
+// const archiveLink = document.getElementById("archiveLink");
+// const archiveText = document.getElementById("archiveText");
+// archiveLink.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   archiveText.classList.toggle("archiveText");
+// });
 
-const facilitationLink = document.getElementById("facilitationLink");
-const facilitationText = document.getElementById("facilitationText");
-facilitationLink.addEventListener("click", (e) => {
-  e.preventDefault();
-  facilitationText.classList.toggle("facilitationText");
-});
+// const facilitationLink = document.getElementById("facilitationLink");
+// const facilitationText = document.getElementById("facilitationText");
+// facilitationLink.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   facilitationText.classList.toggle("facilitationText");
+// });
